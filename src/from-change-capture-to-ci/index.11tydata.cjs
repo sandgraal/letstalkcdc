6501556856b472e741fd6c2de684bedf6fb1b0d4
sandgraal <92229510+sandgraal@users.noreply.github.com>
2025-10-22 15:24:52 -0600

    Add hero config for From Change Capture to Continuous Integration

---
+++ src/from-change-capture-to-ci/index.11tydata.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  heroConfig: {
+    title: "Let’s Talk CDC #7 — From Change Capture to Continuous Integration",
+    description: "<p>How treating data pipelines like code bridges the gap between CDC and CI/CD.</p>",
+    align: 'center',
+    actions: []
+  }
+};
