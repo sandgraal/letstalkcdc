3475793d3520266bf1eb21126d305dd7138154f5
sandgraal <92229510+sandgraal@users.noreply.github.com>
2025-10-23 17:42:55 -0600

    Add handler migration test coverage and JSON response header

--- netlify/functions/migrateUser.js
+++ netlify/functions/migrateUser.js
@@ -127,6 +127,7 @@ const {
 const jsonResponse = (statusCode, body) => ({
   statusCode,
   headers: {
+    "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Methods": "POST,OPTIONS",
